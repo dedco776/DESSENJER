56f3c557581885e980f89255c1fc5aaeea8cd864
dedco776 <diyorbekghd@gmail.com>
2026-08-25 22:17:39 +0500

    Initialize Supabase client in api.js

---
+++ api/api.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL = "https://itdbusdpkgcxyxzbfrjn.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0ZGJ1c2Rwa2djeHl4emJmcmpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2NzU0NjIsImV4cCI6MjEwMzI1MTQ2Mn0.rBOF9muR_S2xba_8x-6AObMOs7clvyleKG9c8TnZYAc";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("DESSENJER: Supabase connected!");
